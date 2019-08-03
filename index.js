25dc93b4bfc98a17ba534665462eddf505d24d87
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-08-03 02:46:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,11 +3,19 @@ class Circle {
 
   constructor(radius) {
     this.radius = radius
-
+    
   }
 
   get diameter() {
     return this.radius * 2;
+  }
+
+  get circumference() {
+    return 2 * Math.PI * this.radius;
+  }
+
+  get area() {
+    return Math.PI * this.radius * this.radius;
   }
 
 

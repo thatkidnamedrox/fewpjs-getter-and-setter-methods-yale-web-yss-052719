7f8f17cf2fabfa6ab08fc14fe3a2422e43295f23
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-08-03 02:53:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Add your Circle class here
+class Circle {
+
+  constructor(radius) {
+    this.radius = radius
+
+  }
+
+  get diameter() {
+    return this.radius * 2;
+  }
+
+  get circumference() {
+    return 2 * Math.PI * this.radius;
+  }
+
+  get area() {
+    return Math.PI * this.radius * this.radius;
+  }
+
+  set diameter(n) {
+    this.radius = n / 2;
+  }
+
+  set circumference(n) {
+    this.radius = n / (2 * Math.PI)
+  }
+
+
+
+
+}

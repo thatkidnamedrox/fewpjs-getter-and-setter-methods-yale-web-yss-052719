97537df80d47383a552c09789b4fe72354f3189b
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-08-03 02:41:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ class Circle {
   constructor(radius) {
     this.radius = radius
     this.diameter = radius * 2;
-    this.circumference = 
+    this.circumference =
     this.area = 2 * Math.PI * radius;
   }
 
